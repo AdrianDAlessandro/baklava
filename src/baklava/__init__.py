e2d231263f73ac3b84616d5f169a759ee74ff52b
Adrian D'Alessandro <a.dalessandro@imperial.ac.uk>
2024-09-03 14:02:05 +0100

    Initial Commit

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Adrian D'Alessandro"
+__email__ = "a.dalessandro@imperial.ac.uk"
+__version__ = "0.1.0"
